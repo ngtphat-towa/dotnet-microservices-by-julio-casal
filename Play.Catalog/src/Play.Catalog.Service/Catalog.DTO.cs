546e314e1a15686d95e72ad89f892fa42ef66bb9
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-10-17 13:34:50 +0700

    Add `Data annotations` into `DTOs`

--- Play.Catalog/src/Play.Catalog.Service/Catalog.DTO.cs
+++ Play.Catalog/src/Play.Catalog.Service/Catalog.DTO.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Play.Catalog.Service.DTOs
 {
     public record ItemDTO(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-    public record CreateItemDTO(string Name, string Description, decimal Price);
-    public record UpdateItemDTO(string Name, string Description, decimal Price);
+    public record CreateItemDTO([Required]string Name, string Description,[Range(0,1000)] decimal Price);
+    public record UpdateItemDTO([Required]string Name, string Description,[Range(0,1000)] decimal Price);
 
 }
