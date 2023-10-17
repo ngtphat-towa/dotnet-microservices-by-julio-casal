a348e7324c5833283bf65c383bd947c6f746a00c
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-10-17 15:05:56 +0700

    fix: data type of fields in `Item` entity

--- Play.Catalog/src/Play.Catalog.Service/Entities/Item.Entity.cs
+++ Play.Catalog/src/Play.Catalog.Service/Entities/Item.Entity.cs
@@ -2,10 +2,10 @@ namespace Play.Catalog.Service.Entities;
 
 public class Item
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
-    public string Price { get; set; } = "";
+    public decimal Price { get; set; }
     public DateTimeOffset CreatedDate { get; set; }
 
 }
