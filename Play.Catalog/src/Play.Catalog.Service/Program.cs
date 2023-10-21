3caa7fd73bcf16a99405bd0428c8cc8b738dac3e
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-10-21 22:47:49 +0700

    add: Refactoring MassTransit configuration into the reusable NuGet package

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,34 +1,30 @@
 
-using MassTransit;
-using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
 using Play.Common.Extensions;
 using Play.Common.Settings;
-
+using Play.Common.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Services: Dependencies
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+builder.Configuration
+    .GetSection(nameof(ServiceSettings))
+    .Get<ServiceSettings>();    
 
 builder.Services
     .AddMongoDatabase()
-    .AddMongoRepository<Item>("items");
+    .AddMongoRepository<Item>("items")
+    .AddMassTransitWithRabbitMQ();
 
-var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        configurator.Host(rabbitMQSettings?.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings?.ServiceName, false));
-    });
-});
+
+
 
 /// Services: Controllers
-builder.Services.AddControllers(
-    options => { options.SuppressAsyncSuffixInActionNames = false; }
+builder.Services
+    .AddControllers(options =>
+    {
+        options.SuppressAsyncSuffixInActionNames = false;
+    }
 );
 
 /// Services: EndpointsApiExplorer
